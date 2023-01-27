e3a52185de1961dea7e2709d6c12756606e27a2b
ANANYA TRIPATHI <ananyatrip0802@gmail.com>
2023-01-27 23:19:12 +0530

    change Homepage

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,22 +1,22 @@
 import React from "react";
+// import avtar from "./EducationSection/avatar.png";
 export const About = () => {
   return (
-    <div className="min-h-screen flex md:ml-44 md:justify-start justify-center ">
-      <div className="about ">
-        <p className="aboutHeading">
-          I am Ananya Tripathi, welcome to my profile...
-        </p>
-        <p>
+    <div className="min-h-screen">
+      <div className="about">
+        <p className="text-6xl font-bold">Hi I am Ananya,</p>
+        <p className="text-2xl">Welcome to my profile...</p>
+        <p className="subText">
           I am an aspiring Full-Stack developer currently learning Back-End and
           polishing my Front-End skills.
         </p>
+        <p className="subText">
+          I have previously worked with React, Javascript, HTML and CSS.
+          Currently learning to work with Node, Express.js
+        </p>
       </div>
       {/* <div>
-        <img
-          className="h-40 w-20"
-          alt="avatar"
-          src="src\Component\avatar.png"
-        />
+        <img src={avtar} alt="avatar" />
       </div> */}
     </div>
   );
--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -2,22 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
-    <div className="h-32 md:px-20 md:flex pt-20 justify-between mb-5">
-      <div className="flex justify-center md:justify-between mx-4">
-        <p className="name">Ananya</p>
-      </div>
-
-      <div className="flex md:mr-[100px] justify-center">
-        <Link to={"/home"}>
-          <p className="routeText">Home</p>
-        </Link>
-        <Link to={"/education"}>
-          <p className="routeText ">Education</p>
-        </Link>
-        <Link to={"/contact"}>
-          <p className="routeText ">Contact Me</p>
-        </Link>
-      </div>
+    <div className="h-24 md:px-20 pt-14 md:flex grid justify-self-end">
+      <Link to={"/home"}>
+        <p className="routeText">Home</p>
+      </Link>
+      <Link to={"/education"}>
+        <p className="routeText ">Education</p>
+      </Link>
+      <Link to={"/contact"}>
+        <p className="routeText ">Contact Me</p>
+      </Link>
     </div>
   );
 };
