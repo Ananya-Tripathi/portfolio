dd513f4db9c77d14851ff3394d6db4969b179738
Ananya Tripathi <85894546+Ananya-Tripathi@users.noreply.github.com>
2024-01-07 21:30:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Experience />
           <Projects />
           <Skills />
-          <a href="https://drive.google.com/file/d/1QjDAfqWi-7t7b7cVfTttclC03b5iTM39/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/1LRNo5HBgFUQPUOuK_6rKn-7whRVYK6rf/view?usp=drive_link">
             <p className="transition-all ease-in-out hover:cursor-pointer text-xl w-[200px] hover:w-[250px] mx-auto border-b-2 my-20 hover:font-bold font-semibold text-center border-2 py-auto ">
               RESUME
             </p>
